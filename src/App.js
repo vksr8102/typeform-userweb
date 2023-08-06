5b5c72658a2e1b32d5aad3ed809c6c0842226db5
vksr8102 <116914527+vksr8102@users.noreply.github.com>
2023-08-07 04:40:17 +0530

    home

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
-import logo from './logo.svg';
 import './App.css';
-import { Box } from '@mui/material';
-import Home from './pages/Home';
 import { Route,BrowserRouter as Router, Routes } from "react-router-dom"
 import Form from './pages/form/Form';
 import Preview from './pages/preview/Preview';
